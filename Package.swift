7b7039c40ad9c039ac852db6cc0cc28fb4b108cd
Gregory Higley <greg@prosumma.com>
2025-10-04 19:37:49 -0400

    Swift 6.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/Prosumma/Parsimonious",
-      from: "2.5.2"
+      from: "2.6.0"
     ),
   ],
   targets: [
